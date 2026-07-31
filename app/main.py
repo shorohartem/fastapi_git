bcd2fd13d5480b09dec151acdb1ab8544337eb6f
shorohartem <shoroh_artem@mail.ru>
2026-08-01 01:01:30 +0300

    fix yml

--- app/main.py
+++ app/main.py
@@ -3,27 +3,30 @@ from fastapi import FastAPI
 from app.core.config import settings
 from app.api.router import router   # если роутер есть — импортируй его
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    print("Starting FastAPI OCR Service...")
-    app.state.settings = settings
-    yield
-    print("Shutting down FastAPI OCR Service...")
+from fastapi import FastAPI
 
-def create_app() -> FastAPI:
-    app = FastAPI(
-        title="OCR Service",
-        docs_url="/docs",
-        description="Анализ изображений через Tesseract и отправка уведомлений",
-        version="1.0.0",
-        lifespan=lifespan
-    )
-    app.include_router(router)
-    return app
-
-app = create_app()
+app = FastAPI(title="OCR Service", docs_url="/docs")
 
 @app.get("/health")
 async def health_check():
-    return {"status": False}
+    return {"status": True}
+
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     print("Starting FastAPI OCR Service...")
+#     app.state.settings = settings
+#     yield
+#     print("Shutting down FastAPI OCR Service...")
+
+# def create_app() -> FastAPI:
+#     app = FastAPI(
+#         title="OCR Service",
+#         docs_url="/docs",
+#         description="Анализ изображений через Tesseract и отправка уведомлений",
+#         version="1.0.0",
+#         lifespan=lifespan
+#     )
+#     app.include_router(router)
+#     return app
+
 
